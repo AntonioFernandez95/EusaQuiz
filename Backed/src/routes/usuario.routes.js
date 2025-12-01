48b90459d1a883aa7c659db56b6efef429980588
Antonio Terri95 <antonioterri95@#gmail.com>
2025-12-01 14:03:50 +0100

    backed

--- Backed/src/routes/usuario.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const usuarioController = require('../controllers/usuario.controller');
-
-router.get('/', usuarioController.obtenerTodos);
-router.post('/', usuarioController.crear);
-
-module.exports = router;
