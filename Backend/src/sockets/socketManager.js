b78aea91ee377b9097a5edbad9cc9946f228aa88
fran33 <franciscomejias.24@campuscamara.es>
2025-12-02 14:49:48 +0100

    feat: Creado FrontEnd para probar

--- Backend/src/sockets/socketManager.js
+++ Backend/src/sockets/socketManager.js
@@ -1,15 +1,20 @@
-// src/sockets/socketManager.js
-
 module.exports = (io) => {
     
-    // Middleware de autenticación (Lo implementaremos más adelante)
-    // io.use((socket, next) => { ... });
-
     io.on('connection', (socket) => {
         console.log(`⚡ Nuevo cliente conectado: ${socket.id}`);
 
-        // Aquí importaremos los handlers específicos más adelante
-        // gameHandler(io, socket);
+        // --- ESTO ES LO QUE TE FALTABA ---
+        // Escuchar cuando un cliente (Profe o Alumno) quiere unirse a una sala
+        socket.on('join_room', (room) => {
+            // El método .join() es nativo de Socket.io y mete al socket en ese canal
+            socket.join(room); 
+            
+            console.log(`✅ Socket ${socket.id} se ha unido a la sala: ${room}`);
+            
+            // Opcional: Responder al cliente confirmando que entró
+            socket.emit('joined_room', { room });
+        });
+        // ---------------------------------
 
         socket.on('disconnect', () => {
             console.log(`❌ Cliente desconectado: ${socket.id}`);
